12258119eea9d4a1446b1b3c8837ccb580cf64b5
pastre <pastre68@gmail.com>
2020-09-03 16:29:04 -0300

    Refactored methods so that they fit their classes

--- ebanxtest/api/views.py
+++ ebanxtest/api/views.py
@@ -40,55 +40,50 @@ def withdraw(accountId, amount):
 	return account
 
 
-def getBalance(pk):
-	account = getAccount(pk)
-	if not account: return notFound('0')
-
-	return HttpResponse(account.balance)
-def onDeposit(event):
-	destination = event.get('destination', False)
-	if not destination: return malformedRequest()
-
-	amount = event.get('amount', False)
-	if not amount: return malformedRequest()
-
-	return HttpResponse(json.dumps({
-		"destination": deposit(destination, amount).toDict()
-		}), status = 201)
-def onWithdraw(event):
-	origin = event.get('origin', False)
-	if not origin: return malformedRequest()
-
-	amount = event.get('amount', False)
-	if not amount: return malformedRequest()
-
-	success = withdraw(origin, amount)
-	if not success: return notFound("0")
-
-	return HttpResponse(json.dumps({
-		"origin": success.toDict()
-		}), status = 201)
-def onTransfer(event):
-	originId = event.get('origin', False)
-	if not originId: return malformedRequest()
-
-	destinationId = event.get('destination', False)
-	if not destinationId: return malformedRequest()
-
-	amount = event.get('amount', False)
-	if not amount: return malformedRequest()
-
-	originAccount = withdraw(originId, amount)
-	if not originAccount: return notFound('0')
-
-	destinationAccount = deposit(destinationId, amount)
-
-	return HttpResponse(json.dumps ({
-		"origin": originAccount.toDict(),
-		"destination": destinationAccount.toDict(),
-		}), status = 201)
 
 class Event(View) :
+	def onDeposit(self, event):
+		destination = event.get('destination', False)
+		if not destination: return malformedRequest()
+
+		amount = event.get('amount', False)
+		if not amount: return malformedRequest()
+
+		return HttpResponse(json.dumps({
+			"destination": deposit(destination, amount).toDict()
+			}), status = 201)
+	def onWithdraw(self, event):
+		origin = event.get('origin', False)
+		if not origin: return malformedRequest()
+
+		amount = event.get('amount', False)
+		if not amount: return malformedRequest()
+
+		success = withdraw(origin, amount)
+		if not success: return notFound("0")
+
+		return HttpResponse(json.dumps({
+			"origin": success.toDict()
+			}), status = 201)
+	def onTransfer(self, event):
+		originId = event.get('origin', False)
+		if not originId: return malformedRequest()
+
+		destinationId = event.get('destination', False)
+		if not destinationId: return malformedRequest()
+
+		amount = event.get('amount', False)
+		if not amount: return malformedRequest()
+
+		originAccount = withdraw(originId, amount)
+		if not originAccount: return notFound('0')
+
+		destinationAccount = deposit(destinationId, amount)
+
+		return HttpResponse(json.dumps ({
+			"origin": originAccount.toDict(),
+			"destination": destinationAccount.toDict(),
+			}), status = 201)
 
 	def post(self, request):
 		asDict = requestToJson(request)
@@ -96,17 +91,24 @@ class Event(View) :
 		event = asDict.get("type", False)
 		if not event: return malformedRequest()
 
-		if event == "deposit": return onDeposit(asDict)
-		if event == "withdraw": return onWithdraw(asDict)
-		if event == "transfer": return onTransfer(asDict)
-		
+		if event == "deposit": return self.onDeposit(asDict)
+		if event == "withdraw": return self.onWithdraw(asDict)
+		if event == "transfer": return self.onTransfer(asDict)
+
 		return malformedRequest()
 class Balance(View):
+	def getBalance(self, pk):
+		account = getAccount(pk)
+		if not account: return notFound('0')
+
+		return HttpResponse(account.balance)
+
+
 	def get(self, request):
 		accountId = request.GET.get("account_id", False)
 		if not accountId: return malformedRequest()
 
-		return getBalance(accountId)
+		return self.getBalance(accountId)
 class Reset(View):
 
 	def clear_db(self): Account.objects.all().delete()
