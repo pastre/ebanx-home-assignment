64342e0d95c10a97e2a2d575c981fd2c2eba16e4
pastre <pastre68@gmail.com>
2020-09-03 14:55:49 -0300

    Created basic event endpoint

--- ebanxtest/api/views.py
+++ ebanxtest/api/views.py
@@ -1,3 +1,50 @@
 from django.shortcuts import render
+from django.view import View
+import json
 
-# Create your views here.
+def requestToJson(request):
+	print("BODY IS", request.body)
+	return json.loads(request.body)
+
+def stringToInt(i):
+	try: return Int(i)
+	except ValueError: return False
+
+def onDeposit(destination, amount):
+	pass # TODO
+
+
+def malformedRequest(): return "TODO"
+
+class Event(View) :
+
+	def sanitizeEvent(event):
+		minimumKeys = ["destination", "amount"]
+		intKeys = ["amount", "balance"]
+
+		for key in minimumKeys: 
+			if not key in event.keys(): return False
+
+		for key, value in event.items():
+			if key in intKeys:
+				number = stringToInt(value)
+				if not number: return False
+				event[key] = number
+
+		return event
+
+
+	def post(self, request):
+		asDict = requestToJson(request)
+
+		event = asDict.get("event", False)
+		if not event: return malformedRequest()
+
+		sanitized = sanitizeEvent(event)
+		if not sanitized: return malformedRequest()
+
+		if event == "deposit": return onDeposit(sanitized["destination"], sanitized["amount"])
+
+
+
+
